d0377cc2e3b3d0fbe9bc02cdb9b33365fffd7364
Sam Castle <sam.gordon.castle@gmail.com>
2017-03-15 21:31:36 -0700

    Adding minor changes to "jQuery Functions" assignment

--- jquery_functions/jquery.js
+++ jquery_functions/jquery.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    
+    $('.click').click(function(){
+        alert("You've clicked the button!");
+    })
+    $('.hide button').click(function(){
+        $('.hide p').hide(1000);
+    })
+    $('.show button').click(function(){
+        $('.show .inivisible').show();
+    })
 })
